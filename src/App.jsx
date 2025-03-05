b5b8abf1e8cbd92a7fa23e869e0eef8f8838cd8a
colt <drownworb@gmail.com>
2025-03-05 16:09:56 +0800

    handling project creation and updating the UI

--- src/App.jsx
+++ src/App.jsx
@@ -21,19 +21,21 @@ function App() {
 
   function handleAddProject(projectData) {
     setProjectState((prevState) => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        id: projectId,
       };
 
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
   }
 
-  console.log(projectState);
+  // console.log(projectState);
 
   let content;
   if (projectState.selectedProjectId === null) {
@@ -44,7 +46,10 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectState.projects}
+      />
       {content}
     </main>
   );
